04f27b16cfacc03ecd4f5d0d6f6bb4ab26903692
Iachi960 <36660843+Iachi960@users.noreply.github.com>
2018-02-26 14:40:31 +0100

    Update CaixerAutomatic.py

--- iticBank/src/CaixerAutomatic.py
+++ iticBank/src/CaixerAutomatic.py
@@ -29,22 +29,31 @@ import time
 
 
 class BankAccount(object):
+    def __guardarcompte(self):
+        f1=open(self.id, "w")
+        n=self.id+"&"+str(self.balance)+"&"+str(self.ianual)+"&"+str(self.carrega)
+        f1.write(n+"\n")
+    def split1(id):
+        f1=open(id, "r")
+        a=f.read()
+        a=a.split("&")
+        return c=BankAccount(a[0],a[1],a[2],a[3])
+
 
     def __canviarstatus(self):
-        self.status = self.balance >= 25
-
+        self.status=self.balance>=25
     def __reinicialitzar(self):
-        self.numWithdrawals = 0
-        self.numDeposits = 0
-
+        self.numWithdrawals=0
+        self.numDeposits=0
     def __init__(self, id, balance, interestRate, monthlyServicesChange):
-        self.ianual = interestRate
-        self.id = id
-        self.balance = balance
+        self.ianual=interestRate
+        self.id=id
+        self.balance=balance
         self.__canviarstatus()
-        self.numDeposits = 0
-        self.numWithdrawals = 0
-        self.carrega = monthlyServicesChange
+        self.numDeposits=0
+        self.numWithdrawals=0
+        self.carrega=monthlyServicesChange
+        self.__guardarcompte()
 
     def withdraw(self, amount):
         """
@@ -61,12 +70,13 @@ class BankAccount(object):
         >>> print BA.balance
         10
         """
-        if self.status == False:
+        if self.status==False:
             return False
         else:
-            self.balance -= amount
-            self.numWithdrawals += 1
+            self.balance-=amount
+            self.numWithdrawals+=1
             self.__canviarstatus()
+            self.__guardarcompte()
             return True
 
     def deposit(self, amount):
@@ -83,23 +93,18 @@ class BankAccount(object):
         30
 
         """
-        self.balance += amount
+        self.balance+=amount
         self.__canviarstatus()
-        self.numDeposits += 1
-
+        self.numDeposits+=1
+        self.__guardarcompte()
     def __str__(self):
         """
         """
         if self.status:
-            a = "Deposits # =" + str(self.numDeposits) + " Withdrawals# =" + str(self.numWithdrawals)
+            a="Deposits # ="+str(self.numDeposits)+" Withdrawals# ="+str(self.numWithdrawals)
         else:
-            a = "INACTIU"
-        return "Data: " + time.strftime("%X") + " " + time.strftime(
-            "%x") + "\n" + "CompteBancari: Codi IBAN: " + self.id[:4] + " Entitat: " + self.id[
-                                                                                       3:8] + " Oficina: " + self.id[
-                                                                                                             8:12] + " num Compte: " + self.id[
-                                                                                                                                       12:] + ": " + str(
-            float(self.balance)) + " " + a
+            a="INACTIU"
+        return "Data: "+ time.strftime("%X")+" "+time.strftime("%x")+"\n"+"CompteBancari: Codi IBAN: "+self.id[:4]+" Entitat: "+self.id[3:8]+" Oficina: "+self.id[8:12]+" num Compte: "+self.id[12:]+": "+str(float(self.balance))+" "+a
 
     def calcMonthlyInterest(self):
         """
@@ -110,8 +115,8 @@ class BankAccount(object):
         >>> print c2.balance
         10.0208333333
         """
-        imensual = self.ianual / 12.
-        self.deposit(imensual * self.balance)
+        imensual=self.ianual/12.
+        self.deposit(imensual*self.balance)
 
     def monthlyProcess(self):
         """
@@ -129,14 +134,15 @@ class BankAccount(object):
         >>> print c1.numDeposits
         0
         """
-        n = 0
-        if self.numWithdrawals > 4:
-            n = self.numWithdrawals - 4
-        self.balance -= self.carrega
+        n=0
+        if self.numWithdrawals>4:
+            n=self.numWithdrawals-4
+        self.balance-=self.carrega
         self.calcMonthlyInterest()
-        self.balance -= n
+        self.balance-=n
         self.__canviarstatus()
         self.__reinicialitzar()
+        self.__guardarcompte()
 
 def opcions():
 
